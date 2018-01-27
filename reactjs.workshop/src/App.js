f7996639226a9c6e1e2eb214a5062c9b94edc2e5
Georgia Tardy <georgiatardyart@gmail.com>
2018-01-27 12:26:24 -0500

    adding components

---
+++ reactjs.workshop/src/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import './App.css';
+import PageTemplate from './components/PageTemplate';
+const App = props => <PageTemplate>Hello World!</PageTemplate>;
+export default App;
